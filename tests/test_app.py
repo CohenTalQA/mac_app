d5a1386ca92a2ac7d880ff8b4a1d1df49265b84d
Tal Cohen <cohe_tal@maccabi-dent.com>
2026-03-09 09:37:47 +0200

    finish cancel feature

--- tests/test_app.py
+++ tests/test_app.py
@@ -36,39 +36,48 @@ def test_login():
     year = current_date.year
     try:
         # login flow
-        # login_page.click_login()
-        # login_page.enter_id(user["id"])
-        # login_page.open_birth_picker()
-        # login_page.pick_birth_year("2010",user["year"]) # think how to fix this hardcoded year, maybe add it to the config file
-        # login_page.pick_birth_month(str(month),user["month"])
-        # login_page.pick_birth_day(str(day),user["day"])
-        # login_page.confirm_birth_date()
-        # login_page.click_send_code()
-        # otp = otp_service.wait_for_otp()
-        # otp_service.enter_otp(otp)
-        # close_popups_if_exist(driver,3)
+        login_page.click_login()
+        login_page.enter_id(user["id"])
+        login_page.open_birth_picker()
+        login_page.pick_birth_year("2010",user["year"]) # think how to fix this hardcoded year, maybe add it to the config file
+        login_page.pick_birth_month(str(month),user["month"])
+        login_page.pick_birth_day(str(day),user["day"])
+        login_page.confirm_birth_date()
+        login_page.click_send_code()
+        otp = otp_service.wait_for_otp()
+        otp_service.enter_otp(otp)
+        close_popups_if_exist(driver,3)
+
+
+
+
+
         # new appointment flow
-        # appointment_page.enter_appointment_page()
-        # appointment_page.dent_test_appointment()
-        # appointment_page.regular_appointment()
-        # appointment_page.choose_city()
-        # appointment_page.select_city()
-        # appointment_page.select_clinic()
-        # appointment_page.choose_doctor()
-        # appointment_page.select_first_available_time()
-        # appointment_page.confirm_appointment()
-        # appointment_page.finish_appointment()
-        # edit appointment flow
-        # my_appointment_page.open_appointment_page()
-        # my_appointment_page.select_appointment_type()
-        # my_appointment_page.edit_appointment_type()
-        # # second_option_popup_if_exist(driver)
-        # appointment_page.select_first_available_time()
-        # appointment_page.confirm_appointment()
+        appointment_page.enter_appointment_page()
+        appointment_page.dent_test_appointment()
+        appointment_page.regular_appointment()
+        appointment_page.choose_city()
+        appointment_page.select_city()
+        appointment_page.select_clinic()
+        appointment_page.choose_doctor()
+        appointment_page.select_first_available_time()
+        appointment_page.confirm_appointment()
+        appointment_page.finish_appointment()
+        # # edit appointment flow
+        my_appointment_page.open_my_appointment_page()
+        my_appointment_page.select_my_appointment()
+        my_appointment_page.edit_my_appointment()
+        second_option_popup_if_exist(driver)
+        appointment_page.select_first_available_time()
+        appointment_page.confirm_appointment()
         appointment_page.finish_appointment()
         #cancel appointment flow
-        # my_appointment_page.select_appointment_type()
-
-
+        my_appointment_page.select_my_appointment()
+        my_appointment_page.cancel_my_appointment()
+        second_option_popup_if_exist(driver)
+        appointment_page.select_first_available_time()
+        appointment_page.confirm_appointment()
+        appointment_page.finish_appointment()
+        my_appointment_page.reutrn_home()
     finally:
         driver.quit()
